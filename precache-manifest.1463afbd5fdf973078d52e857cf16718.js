0a3daebf8bbf26fe1c18cbdad7ed82c5959a1305
Mauricio Barbier <mauriciobarbier@Mauricios-MBP.attlocal.net>
2020-04-04 14:46:41 -0500

    Updates

---
+++ precache-manifest.1463afbd5fdf973078d52e857cf16718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d63859944b0b7b4ed7ff65f26c3be96",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "7e43f17cdc8203fdd2b8",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c7501d0d55ed370869b0",
+    "url": "/react-test/static/js/2.b76209aa.chunk.js"
+  },
+  {
+    "revision": "b532e876c277c3b39013a883ebad2390",
+    "url": "/react-test/static/js/2.b76209aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e43f17cdc8203fdd2b8",
+    "url": "/react-test/static/js/main.9eb9043b.chunk.js"
+  },
+  {
+    "revision": "445988191edebfd34f12",
+    "url": "/react-test/static/js/runtime-main.011e1daf.js"
+  }
+]);
